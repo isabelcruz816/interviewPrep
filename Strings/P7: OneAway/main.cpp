36184ff4b5c2de65386a0cf81fa6ce319c96a705
Isabel Cruz <anaisabelcruz@Anas-MacBook-2.local>
2019-10-13 14:29:39 -0500

    p7: is one away

--- Strings/P7: OneAway/main.cpp
+++ Strings/P7: OneAway/main.cpp
@@ -6,32 +6,32 @@ using namespace std;
 bool isOneAway(string first, string second)
 {
     int lengthdiff = first.length() - second.length();
+    bool difference = 0;
+
     if (abs(lengthdiff) > 1)
         return false;
 
-    // s1 is my shortest string and s2 is the longest string
-    string s1 = first.length() < second.length() ? first : second;
-    string s2 = first.length() < second.length() ? second : first;
-
-    bool difference = 0;
-
-    if (s1.length() == s2.length())
+    if (first.length() == second.length())
     {
         // They can be one replacement away
-        for (int i = 0; i < s1.length(); i++)
+        for (int i = 0; i < first.length(); i++)
         {
-            if (s1[i] != s2[i])
+            if (first[i] != second[i])
             {
                 if (difference)
                     return false;
                 difference = 1;
             }
         }
+        return true;
     }
     else
     {
+        // s1 is my shortest string and s2 is the longest string
+        string s1 = first.length() < second.length() ? first : second;
+        string s2 = first.length() < second.length() ? second : first;
         int index1 = 0, index2 = 0;
-        // They can be one insert or remove away
+
         while (index1 < s1.length() && index2 < s2.length())
         {
             if (s1[index1] == s2[index2])
@@ -48,7 +48,6 @@ bool isOneAway(string first, string second)
             }
         }
     }
-
     return true;
 }
 
